5a8037a94ac24ee3c32527f3e8fcdc478ea7a5eb
Ezeokeke Collins Chidubem <90806850+CollinsEzeokeke@users.noreply.github.com>
2024-07-13 06:08:07 -0700

    final updates
    made a middleware to make the application more organized

--- src/route.ts
+++ src/route.ts
@@ -1,36 +1,8 @@
 import { Express, Request, Response } from "express";
+import getBook from "./books/controllers/books.controller";
 
 export default function routes(app: Express){
     
 app.get(
-    "/api/books/:bookId/:authorId",
-    function (
-      req: Request<
-        { bookId: string; authorId: string },
-        {},
-        { name: string },
-        {}
-      >,
-      res: Response
-    ) {
-      req.body;
-      console.log(res.locals.name);
-      return res.send(res.locals.name);
-    }
-  );
-  
-  async function throwsError() {
-      throw new Error('Close this stuff');
-  }
-  
-  //Error handling
-  
-  app.get('/error', async (req: Request, res: Response) => {
-      try {
-          await throwsError();
-          res.sendStatus(200);
-      } catch (error) {
-          res.status(400).send(`This is your error:${error}`);
-      }
-  });
+    "/api/books/:bookId/:authorId",getBook);
 }
